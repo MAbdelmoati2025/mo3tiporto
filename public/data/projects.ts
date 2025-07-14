72dce687cddffac55fd5609428643075a1fcafeb
MAbdelmoati2025 <142133829+MAbdelmoati2025@users.noreply.github.com>
2025-07-14 08:02:41 +0300

    Updated projects.ts

--- public/data/projects.ts
+++ public/data/projects.ts
@@ -3,7 +3,7 @@ export const projects = [
     // To use your own project photo, just place it in /public/assets and write the link as I did: eg: /assets/my_image.jpg
     // Of course it's best to convert your image file type to webp for better performance on the web!
     // Or just use a cloud image hosting platform like cloudinary and use the image url from there.
-    ImageURL: "/assets/project-eshterely.webp", //
+    ImageURL: "/assets/Screenshot 2025-07-14 075843.png", //
     Title: "Smart Garage System",
     Description:
       "This project is a smart garage door automation system built using Arduino and an ultrasonic sensor. When a car approaches the garage within a certain distance, the ultrasonic sensor detects its presence. The Arduino then activates a servo motor or motor driver to open the garage door. After a short delay (allowing the car to enter), the door automatically closes.This system increases convenience, enhances security, and reduces manual effort, making it ideal for smart home applications.",
