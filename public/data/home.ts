4018a59aabefa2c3ecc13bcb1a3abdcad815e147
MAbdelmoati2025 <142133829+MAbdelmoati2025@users.noreply.github.com>
2025-07-14 07:32:09 +0300

    Updated home.ts

--- public/data/home.ts
+++ public/data/home.ts
@@ -10,34 +10,22 @@ export const homeData = {
   // To use your own photo, just place it in /public/assets and write the link as I did: eg: /assets/my_image.jpg
   // Of course it's best to convert your image file type to webp for better performance on the web!
   // The links are optional
-  myImage: "/assets/my_image.webp",
+  myImage: "/assets/WhatsApp Image 2025-06-22 at 17.04.14_e8e43668.jpg",
   contactInfo: [
     {
       Icon: IoMdMail,
-      Label: "kareem.e.hamouda@gmail.com",
+      Label: "codexacademy50@gmail.com",
       Link: "/contact", // This refers to the Contact page in the website, you can change it to whatever you like
     },
     {
       Icon: FaLocationDot,
-      Label: "Cairo, Egypt",
+      Label: "Cairo, Maadi, Egypt",
       Link: "https://www.google.com/maps/place/Cairo,+Cairo+Governorate/@30.0594885,31.2584644,13z", // This is just a simple location of my city
     },
     {
       Icon: FaPhoneAlt,
-      Label: "+20 115 111 9085",
-      Link: "tel:+201151119085",
-    },
-  ],
-  education: [
-    {
-      Icon: FaUniversity,
-      Label: "Ain Shams University",
-      Link: "https://www.asu.edu.eg/",
-    },
-    {
-      Icon: FaGraduationCap,
-      Label: "Bachelor's Degree in Computer Science",
-      Link: "https://cis.asu.edu.eg/",
+      Label: "+20 1022075809,
+      Link: "tel:+201022075809",
     },
   ],
   social: [
