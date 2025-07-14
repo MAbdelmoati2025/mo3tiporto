752b7cdcb4ab7e9187a89a73165b03dc872e7135
MAbdelmoati2025 <142133829+MAbdelmoati2025@users.noreply.github.com>
2025-07-14 08:11:07 +0300

    Updated home.ts

--- public/data/home.ts
+++ public/data/home.ts
@@ -55,7 +55,6 @@ export const homeData = {
       Link: "https://www.facebook.com/profile.php?id=61572621389592",
     },
     {
-      Icon: FaInstagram,
       Link: "https://www.instagram.com/codex__academy?igsh=bXByeWl4c3VuM3h3",
     },
   ],
