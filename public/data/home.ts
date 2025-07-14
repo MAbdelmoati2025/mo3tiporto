9df29c12f4a44925d1716a4925589f7122987f8f
MAbdelmoati2025 <142133829+MAbdelmoati2025@users.noreply.github.com>
2025-07-14 07:37:28 +0300

    Updated home.ts

--- public/data/home.ts
+++ public/data/home.ts
@@ -25,7 +25,7 @@ export const homeData = {
     {
       Icon: FaPhoneAlt,
       Label: "+20 1022075809",
-      Link: "tel:+201151119085",
+      Link: "tel:+201022075809",
     },
   ],
   education: [
