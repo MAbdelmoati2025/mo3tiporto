fa710b758943ed6aff2668fee426fe0dd2657cad
MAbdelmoati2025 <142133829+MAbdelmoati2025@users.noreply.github.com>
2025-07-14 07:14:37 +0300

    Updated home.ts

--- public/data/home.ts
+++ public/data/home.ts
@@ -1,65 +1,48 @@
 import { IoMdMail } from "react-icons/io";
-import { FaLocationDot } from "react-icons/fa6";
-import { FaPhoneAlt, FaGithub, FaLinkedin } from "react-icons/fa";
-import { FaUniversity } from "react-icons/fa";
-import { FaGraduationCap } from "react-icons/fa6";
-import { FaFacebook } from "react-icons/fa";
-import { FaInstagram } from "react-icons/fa";
+import { FaLocationDot, FaGraduationCap, FaTiktok } from "react-icons/fa6";
+import {
+  FaPhoneAlt,
+  FaGithub,
+  FaLinkedin,
+  FaFacebook,
+  FaInstagram,
+  FaUniversity,
+} from "react-icons/fa";
 
 export const homeData = {
-  // To use your own photo, just place it in /public/assets and write the link as I did: eg: /assets/my_image.jpg
-  // Of course it's best to convert your image file type to webp for better performance on the web!
-  // The links are optional
   myImage: "/assets/WhatsApp Image 2025-06-22 at 17.04.14_e8e43668.jpg",
   contactInfo: [
     {
       Icon: IoMdMail,
-      Label: "Mohamed.a.abdelmoati2020@gmail.com",
-      Link: "/contact", // This refers to the Contact page in the website, you can change it to whatever you like
+      Label: "codexacademy50@gmail.com",
+      Link: "/contact",
     },
     {
       Icon: FaLocationDot,
-      Label: "Cairo, Egypt",
-      Link: "https://www.google.com/maps/place/Cairo,+Cairo+Governorate/@30.0594885,31.2584644,13z", // This is just a simple location of my city
+      Label: "Cairo, Maadi, Egypt",
+      Link: "https://www.google.com/maps/place/Cairo,+Cairo+Governorate/@30.0594885,31.2584644,13z",
     },
     {
       Icon: FaPhoneAlt,
-      Label: "+201014884327",
-      Link: "tel:+201014884327",
-    },
-  ],
-  education: [
-    {
-      Icon: FaUniversity,
-      Label: "Helwan  University",
-      Link: "https://www.asu.edu.eg/",
-    },
-    {
-      Icon: FaGraduationCap,
-      Label: "Bachelor's Degree in Engineering",
-      Link: "https://cis.asu.edu.eg/",
+      Label: "+201022075809",
+      Link: "tel:+201022075809",
     },
   ],
   social: [
     {
-      Icon: FaGithub,
-      Label: "GitHub",
-      Link: "https://github.com/MAbdelmoati2025",
-    },
-    {
-      Icon: FaLinkedin,
-      Label: "LinkedIn",
-      Link: "https://www.linkedin.com/in/mohamed-a-abdelmoati-229688299",
+      Icon: FaTiktok,
+      Label: "TikTok",
+      Link: "https://www.tiktok.com/@codexacademy1?_t=ZS-8xVhNzXXq6R&_r=1",
     },
     {
       Icon: FaFacebook,
       Label: "Facebook",
-      Link: "https://www.facebook.com/MohamedAhmedAbdelmoati1752002/",
+      Link: "https://www.facebook.com/profile.php?id=61572621389592",
     },
     {
       Icon: FaInstagram,
       Label: "Instagram",
-      Link: "https://www.instagram.com/mohamed.a.abdelmoati_7/",
+      Link: "https://www.instagram.com/codex__academy?igsh=bXByeWl4c3VuM3h3",
     },
   ],
 };
