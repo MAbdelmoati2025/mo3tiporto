edac8c9dfa85975a88d23e1e62ae287128542fc4
MAbdelmoati2025 <142133829+MAbdelmoati2025@users.noreply.github.com>
2025-07-14 07:39:59 +0300

    Updated home.ts

--- public/data/home.ts
+++ public/data/home.ts
@@ -1,6 +1,8 @@
 import { IoMdMail } from "react-icons/io";
 import { FaLocationDot } from "react-icons/fa6";
 import { FaPhoneAlt, FaGithub, FaLinkedin } from "react-icons/fa";
+import { FaUniversity } from "react-icons/fa";
+import { FaGraduationCap } from "react-icons/fa6";
 import { FaFacebook } from "react-icons/fa";
 import { FaInstagram } from "react-icons/fa";
 
@@ -8,25 +10,36 @@ export const homeData = {
   // To use your own photo, just place it in /public/assets and write the link as I did: eg: /assets/my_image.jpg
   // Of course it's best to convert your image file type to webp for better performance on the web!
   // The links are optional
-  myImage: "/assets/WhatsApp Image 2025-06-22 at 17.04.14_e8e43668.jpg",
+  myImage: "/assets/my_image.webp",
   contactInfo: [
     {
       Icon: IoMdMail,
-      Label: "codexacademy50@gmail.com",
+      Label: "kareem.e.hamouda@gmail.com",
       Link: "/contact", // This refers to the Contact page in the website, you can change it to whatever you like
     },
     {
       Icon: FaLocationDot,
-      Label: "Cairo, Maadi, Egypt",
+      Label: "Cairo, Egypt",
       Link: "https://www.google.com/maps/place/Cairo,+Cairo+Governorate/@30.0594885,31.2584644,13z", // This is just a simple location of my city
     },
     {
       Icon: FaPhoneAlt,
-      Label: "+20 1022075809",
-      Link: "tel:+201022075809",
+      Label: "+20 115 111 9085",
+      Link: "tel:+201151119085",
     },
   ],
- 
+  education: [
+    {
+      Icon: FaUniversity,
+      Label: "Ain Shams University",
+      Link: "https://www.asu.edu.eg/",
+    },
+    {
+      Icon: FaGraduationCap,
+      Label: "Bachelor's Degree in Computer Science",
+      Link: "https://cis.asu.edu.eg/",
+    },
+  ],
   social: [
     {
       Icon: FaGithub,
