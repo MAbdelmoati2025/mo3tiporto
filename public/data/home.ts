e98f014bacafbc160b127a6f0a66656a89e066f2
MAbdelmoati2025 <142133829+MAbdelmoati2025@users.noreply.github.com>
2025-07-14 07:40:20 +0300

    Updated home.ts

--- public/data/home.ts
+++ public/data/home.ts
@@ -10,7 +10,7 @@ export const homeData = {
   // To use your own photo, just place it in /public/assets and write the link as I did: eg: /assets/my_image.jpg
   // Of course it's best to convert your image file type to webp for better performance on the web!
   // The links are optional
-  myImage: "/assets/my_image.webp",
+  myImage: "/assets/WhatsApp Image 2025-06-22 at 17.04.14_e8e43668.jpg",
   contactInfo: [
     {
       Icon: IoMdMail,
